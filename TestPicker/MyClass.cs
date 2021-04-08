26dd96983c591de1f227fb6c1859ddf2604c6c78
Kiryl Akulich <Kirill.Akulich@softeq.com>
2021-04-08 10:55:13 +0300

    ínit

---
+++ TestPicker/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TestPicker
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ iOS/ViewController.designer.cs
@@ -0,0 +1,34 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace TestPicker.iOS
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UIButton Button { get; set; }
+
+		[Outlet]
+		UIKit.UIDatePicker datepicker { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (Button != null) {
+				Button.Dispose ();
+				Button = null;
+			}
+
+			if (datepicker != null) {
+				datepicker.Dispose ();
+				datepicker = null;
+			}
+		}
+	}
+}
